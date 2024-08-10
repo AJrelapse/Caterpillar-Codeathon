0b8aaf42592efaac3f162d9c0b11bb04df863535
TBA5854 <adhavan5030@gmail.com>
2024-08-10 20:40:46 +0530

    feat: Add nodemailer dependency for email functionality

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { login, logout, signup } from "../controllers/authController"
-import { revokeAdmin, makeUserAdmin } from "../controllers/adminController"
-import { authverify, isAdmin } from "../middleware/authMiddleware";
+// import { revokeAdmin, makeUserAdmin } from "../controllers/adminController"
+// import { authverify, isAdmin } from "../middleware/authMiddleware";
 const router: Router = Router();
 
 router.get('/logout', logout);
 router.post('/login', login);
 router.post('/signup', signup);
-router.get('/adminify', authverify, isAdmin, makeUserAdmin);
-router.get('/deadminify', authverify, isAdmin, revokeAdmin);
-
+// router.get('/adminify', authverify, isAdmin, makeUserAdmin);
+// router.get('/deadminify', authverify, isAdmin, revokeAdmin);
+// add reset password route
+// add forgot password route
 export default router;
